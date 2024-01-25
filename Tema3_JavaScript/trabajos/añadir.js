ac4ab4d30d1300e5d1810ce6108de85adbf38756
DanieBarrio <danielbarrio05@iesamachado.org>
2024-01-25 14:16:25 +0100

    sad

--- Tema3_JavaScript/trabajos/añadir.js
+++ Tema3_JavaScript/trabajos/añadir.js
@@ -1,17 +1,22 @@
 var lista=[];
 
 function aumentarbtt(){
-
-    var texto = document.getElementById("aumentar").innerHTML
+    var texto = document.getElementById("aumentar").innerHTML;
     lista.push(texto);
 
    
     let text = "<ul>";
-    lista.forEach(myFunction);
+    //lista.forEach(myFunction);
+    //text += "</ul>";
+    //document.getElementById("listado").innerHTML = text;
+    let fLen = lista.length;
+    for (let i = 0; i < fLen; i++) {
+      text += "<li>" + i + lista[i] + "</li>";
+    }
     text += "</ul>";
     document.getElementById("listado").innerHTML = text;
 }
 
-function myFunction(value) {
-  text += "<li>" + value + "</li>";
-} 
+//function myFunction(value) {
+//  text += "<li>" + value + "</li>";
+//} 
